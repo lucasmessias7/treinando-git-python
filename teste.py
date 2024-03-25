4da79c9e22a0f56ba337f48b3816bbad87ba993f
lucasmessiasalves <luquinhasmessias7@gmail.com>
2024-03-25 11:08:40 -0300

    att.new

---
+++ teste.py
@@ -0,0 +1,25 @@
+'''alunos = {'pedro': 7.2, 'joao': 9.2, 'lucas':6.7, 'maria': 10, 'rafaela': 5.6 }
+
+soma = sum(alunos.values())
+
+result = soma / len(alunos)
+print(f'{result:.2f}')'''
+'''
+senha = '456328'
+leitor =''
+
+while (leitor != senha):
+    leitor = input('digite a senha: ')
+    if leitor == senha:
+        print('acesso concedido')
+    else:
+        print('tente novamente') '''
+        
+
+contador = 0
+somador = 0
+while contador < 2:
+ contador = contador * 1
+ valor = float(input('Digite o '+str(contador)+'º valor: '))
+ somador = somador + valor
+print('Soma = ', somador)
